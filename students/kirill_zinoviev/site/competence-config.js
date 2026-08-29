a52760e84cb89e1e5bd3bc28f8b3db318d0963f1
ArtemLevin <118441740+ArtemLevin@users.noreply.github.com>
2026-08-29 12:30:24 +0300

    feat(kirill): update competence evidence for 29.08

--- students/kirill_zinoviev/site/competence-config.js
+++ students/kirill_zinoviev/site/competence-config.js
@@ -6,7 +6,8 @@ const lessons=[
 {date:'15.08.26',href:'15.08.26.html',ids:['expr_5','expr_6','equations_4','equations_5','equations_6','equations_12','models_1','models_2','models_3','models_4','models_5','models_6','models_7','models_8','models_14']},
 {date:'19.08.26',href:'19.08.26.html',ids:['expr_5','expr_6','expr_7','equations_2','equations_3','equations_4','equations_5','equations_6','equations_8','equations_12','models_1','models_2','models_3','models_4','models_5','models_9','models_10','models_14']},
 {date:'22.08.26',href:'22.08.26.html',ids:['fractions_14','fractions_15','fractions_16','percent_8','percent_9','percent_11','percent_12','percent_14','expr_1','expr_2','expr_3','models_1','models_2','models_3','models_4','models_5','models_12','models_14']},
-{date:'26.08.26',href:'26.08.26.html',ids:['percent_3','percent_4','percent_9','percent_11','percent_12','percent_13','models_12']}
+{date:'26.08.26',href:'26.08.26.html',ids:['percent_3','percent_4','percent_9','percent_11','percent_12','percent_13','models_12']},
+{date:'29.08.26',href:'29.08.26.html',ids:['calc_3','calc_4','calc_5','calc_6','fractions_8','fractions_9','rational_1','rational_2','rational_3','rational_6','rational_7','rational_10','rational_11','expr_8']}
 ];
 const teacherSeed={},evidence={};for(const lesson of lessons)for(const id of lesson.ids){teacherSeed[id]=Math.max(teacherSeed[id]||0,2);evidence[id]={text:`Тема подтверждена материалом занятия ${lesson.date}.`,href:lesson.href};}
 const source=URL.createObjectURL(new Blob([`const groups=${JSON.stringify(groups)};`],{type:'text/javascript'}));
