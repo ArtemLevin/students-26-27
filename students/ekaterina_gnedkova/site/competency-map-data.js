f43b132777b6c9c8dea21ab2dc07b5ccfceca42c
ArtemLevin <118441740+ArtemLevin@users.noreply.github.com>
2026-09-09 11:40:46 +0300

    Add Ekaterina Gnedkova EGE competency map

---
+++ students/ekaterina_gnedkova/site/competency-map-data.js
@@ -0,0 +1,7 @@
+(() => {
+'use strict';
+for (let i = 1; i <= 4; i += 1) {
+  document.write(`<script src="competency-map-data-part-${i}.js?v=20260909-2"><\/script>`);
+}
+document.write('<script src="competency-map-data-assemble.js?v=20260909-2"><\/script>');
+})();
