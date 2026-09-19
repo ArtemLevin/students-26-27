c863898198327d720e20d315f82971123c2b7105
ArtemLevin <118441740+ArtemLevin@users.noreply.github.com>
2026-09-19 20:53:11 +0300

    feat(sofya): load 19.09 lesson and competence update

--- students/sofya_kalney/site/dashboard.js
+++ students/sofya_kalney/site/dashboard.js
@@ -1,5 +1,5 @@
-import './competence-update-20260913.js?v=20260913-1';
-import {LESSONS} from './lesson-registry.js?v=20260913-1';
+import './competence-update-20260919.js?v=20260919-1';
+import {LESSONS} from './lesson-registry.js?v=20260919-1';
 import {initStudentDashboard} from '../../../shared/student-dashboard/dashboard-core.js';
 import {PRACTICE_CONFIG} from './practice-config.js';
 
