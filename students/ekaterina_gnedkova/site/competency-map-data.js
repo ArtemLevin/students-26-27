691eea850b3a54a32faff9335dc20ab29b16c331
ArtemLevin <118441740+ArtemLevin@users.noreply.github.com>
2026-09-26 08:52:53 +0300

    chore(ekaterina): bust competency map data cache

--- students/ekaterina_gnedkova/site/competency-map-data.js
+++ students/ekaterina_gnedkova/site/competency-map-data.js
@@ -1,7 +1,7 @@
 (() => {
 'use strict';
 for (let i = 1; i <= 4; i += 1) {
-  document.write(`<script src="competency-map-data-part-${i}.js?v=20260922-1"><\/script>`);
+  document.write(`<script src="competency-map-data-part-${i}.js?v=20260925-1"><\/script>`);
 }
-document.write('<script src="competency-map-data-assemble.js?v=20260922-1"><\/script>');
+document.write('<script src="competency-map-data-assemble.js?v=20260925-1"><\/script>');
 })();
